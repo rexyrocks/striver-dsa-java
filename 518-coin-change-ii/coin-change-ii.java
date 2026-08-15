9f743667f63b4c3795f94cd2bba914a55a76a8b2
rexyrocks <piyochoudhary@gmail.com>
2026-08-15 23:31:37 +0530

    Time: 5 ms (99.79%) | Memory: 43.1 MB (77.60%) - LeetSync

--- 518-coin-change-ii/coin-change-ii.java
+++ 518-coin-change-ii/coin-change-ii.java
@@ -5,9 +5,8 @@ class Solution {
         dp[0] = 1;
             for(int coin:coins){
         for(int i =coin ; i <= amount ; i++){
-              if(i-coin >= 0){
-dp[i] = dp[i]+dp[i-coin];
-                }
+       dp[i] = dp[i]+dp[i-coin];
+           
             }
         }
    return dp[amount];
