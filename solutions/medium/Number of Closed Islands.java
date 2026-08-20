28f438768d9d18fe0ce1352f953068e2c671245c
rexyrocks <piyochoudhary@gmail.com>
2026-08-20 22:24:17 +0530

    Add solution for Number of Closed Islands

---
+++ solutions/medium/Number of Closed Islands.java
@@ -0,0 +1,13 @@
+// Title: Number of Closed Islands
+            // Difficulty: Medium
+            // Language: Java
+            // Link: https://leetcode.com/problems/number-of-closed-islands/
+
+        }
+                }
+            }
+        }
+    
+        return count;
+    }
+}
