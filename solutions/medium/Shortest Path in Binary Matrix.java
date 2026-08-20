aaa11211d1bbfe21e24be0cdeb2cc610ee53832b
rexyrocks <piyochoudhary@gmail.com>
2026-08-20 23:39:24 +0530

    Add solution for Shortest Path in Binary Matrix

---
+++ solutions/medium/Shortest Path in Binary Matrix.java
@@ -0,0 +1,13 @@
+// Title: Shortest Path in Binary Matrix
+            // Difficulty: Medium
+            // Language: Java
+            // Link: https://leetcode.com/problems/shortest-path-in-binary-matrix/
+
+            grid[nr][nc] = 1;
+            int x = d+1;
+            queue.add(new int[]{nr,nc,x});
+         }
+        }
+      }
+ return -1;   }
+}
