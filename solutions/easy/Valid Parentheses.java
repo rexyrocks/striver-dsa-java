d35d06faf59a2c927367fba39999479bc1b7e3d8
rexyrocks <piyochoudhary@gmail.com>
2026-08-21 22:15:00 +0530

    Add solution for Valid Parentheses

---
+++ solutions/easy/Valid Parentheses.java
@@ -0,0 +1,13 @@
+// Title: Valid Parentheses
+            // Difficulty: Easy
+            // Language: Java
+            // Link: https://leetcode.com/problems/valid-parentheses/
+
+}
+else if(c == ']'){
+    if(stack.isEmpty() || stack.pop() != '[') return false;
+}
+}
+return stack.isEmpty();
+    }
+}
