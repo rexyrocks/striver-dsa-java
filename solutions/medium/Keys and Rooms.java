30442f72ab6f09438b4b72629709278210b1c291
rexyrocks <piyochoudhary@gmail.com>
2026-08-23 22:54:37 +0530

    Add solution for Keys and Rooms

---
+++ solutions/medium/Keys and Rooms.java
@@ -0,0 +1,13 @@
+// Title: Keys and Rooms
+            // Difficulty: Medium
+            // Language: Java
+            // Link: https://leetcode.com/problems/keys-and-rooms/
+
+        }
+
+    }
+    for(int i = 0 ; i< visited.length; i++){
+        if(visited[i] == false) return false;
+    }
+    return true;
+}}
