8697296cfa51911ce12429ef93501035874d62ba
rexyrocks <piyochoudhary@gmail.com>
2026-09-03 11:54:08 +0530

    Add solution for Min Cost to Connect All Points

--- solutions/medium/Min Cost to Connect All Points.java
+++ solutions/medium/Min Cost to Connect All Points.java
@@ -3,10 +3,4 @@
             // Language: Java
             // Link: https://leetcode.com/problems/min-cost-to-connect-all-points/
 
-        return parents[x];
-    }
-    void union(int x, int y){
-        int rootx = find(x) , rooty = find(y);
-        if(rootx != rooty) parents[rootx] = rooty;
-    }
-}
+
