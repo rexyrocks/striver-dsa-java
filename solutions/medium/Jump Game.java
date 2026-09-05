1e5ad92870a131f9a3579f8a3e2d3599621780be
rexyrocks <piyochoudhary@gmail.com>
2026-09-05 14:52:28 +0530

    Add solution for Jump Game

---
+++ solutions/medium/Jump Game.java
@@ -0,0 +1,13 @@
+// Title: Jump Game
+            // Difficulty: Medium
+            // Language: Java
+            // Link: https://leetcode.com/problems/jump-game/
+
+maxreach = Math.max(maxreach, max);
+            }
+                int max = nums[i] + i;
+        }
+        return true;
+    }
+}
+            else{
